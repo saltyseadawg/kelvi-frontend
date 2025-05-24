e4c3abe1cd5293225f93233909b22268620f7803
saltyseadawg <sabrinayu7@gmail.com>
2025-05-24 10:16:08 -0400

    fix: assign parameter type in navbar chore: removed comments

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -7,7 +7,7 @@ const navigation = [
   { name: 'Contact Us', href: '#', current: false },
 ]
 
-function classNames(...classes) {
+function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
 
