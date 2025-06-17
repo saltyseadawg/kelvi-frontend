9adb6e853c1f94553ddcbcda1df2aa9739d43062
saltyseadawg <sabrinayu7@gmail.com>
2025-06-17 14:56:46 -0400

    feat: add search functionality - add testing libraries

--- src/app/components/Searchbar/Searchbar.test.tsx
+++ src/app/components/Searchbar/Searchbar.test.tsx
@@ -1,9 +1,38 @@
-import { expect, test, describe, it, vi} from 'vitest'
-import {  } from "vitest";
+import { expect, test, vi} from 'vitest'
 import { render, screen } from '@testing-library/react'
 import Searchbar from './Searchbar'
+import { submitSearch } from '@/app/actions/actions'
+
+
+
+vi.mock('@/app/actions/actions')
  
 test('Search bar should appear', () => {
     render(<Searchbar />)
     expect(screen.getByPlaceholderText("Search for a word")).toBeDefined();
 })
+
+test('Pressing submit with user input should pass string to back end', () => {
+    const handleSearch = vi.fn();
+    handleSearch.mockClear()
+
+})
+
+// describe("sampleAction", () => {
+//   test("Normal: Returns the correct value", async () => {
+//     const result = await sampleAction(true);
+//     expect(result).toEqual({ sampleAction: "sampleAction" });
+//   });
+
+//   test("Error: Redirects to the error page", async () => {
+//     vi.mock("next/navigation", () => {
+//       return {
+//         redirect: (path: string) => {
+//           return path;
+//         },
+//       };
+//     });
+//     const result = await sampleAction(false);
+//     expect(result).toBe("/error");
+//   });
+// });
