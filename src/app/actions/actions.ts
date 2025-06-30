192d0d6be4d55c3463a40001f9d4982d6f7a55d0
saltyseadawg <sabrinayu7@gmail.com>
2025-06-29 21:55:39 -0400

    remove unnecessary logging

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -1,9 +1,7 @@
 "use server";
 
 export async function submitSearch(previousState: string, formData: FormData){
-    // await new Promise((resolve) => setTimeout(resolve, 2000))
     const query = formData.get("query") as string
-    console.log(query)
     const response = await fetch(
         `http://127.0.0.1:8000/word/${query}`, {
             method: 'GET',
