ff1f117f9d81c34016f371a12b0ed44a620c4bfb
saltyseadawg <sabrinayu7@gmail.com>
2025-06-29 21:41:05 -0400

    fix method type for api call

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -4,5 +4,15 @@ export async function submitSearch(previousState: string, formData: FormData){
     // await new Promise((resolve) => setTimeout(resolve, 2000))
     const query = formData.get("query") as string
     console.log(query)
-    return query
+    const response = await fetch(
+        `http://127.0.0.1:8000/word/${query}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    )
+    const result = await response.json()
+    console.log(result)
+    return result
 }
