9bfac1429f84eb4d830042734c23e8bffc2b9941
Sorano <sabrinayu7@gmail.com>
2025-06-29 22:00:22 -0400

    add GET request on search submission (#5)

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -1,8 +1,16 @@
 "use server";
 
 export async function submitSearch(previousState: string, formData: FormData){
-    // await new Promise((resolve) => setTimeout(resolve, 2000))
     const query = formData.get("query") as string
-    console.log(query)
-    return query
+    const response = await fetch(
+        `http://127.0.0.1:8000/word/${query}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    )
+    const result = await response.json()
+    console.log(result)
+    return result
 }
