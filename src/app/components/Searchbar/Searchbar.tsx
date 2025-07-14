e7d969656e1100f0bb63c591892eabf045ac9514
saltyseadawg <sabrinayu7@gmail.com>
2025-07-14 14:28:42 -0400

    feat: pass search result from search button to parent component

--- src/app/components/Searchbar/Searchbar.tsx
+++ src/app/components/Searchbar/Searchbar.tsx
@@ -3,8 +3,16 @@
 import { submitSearch } from "@/app/actions/actions"
 import { useActionState } from "react"
 
-export default function Searchbar() {
-    const [state, action] = useActionState(submitSearch, "")
+// export default function ClientComponent({
+//   updateItemAction,
+// }: {
+//   updateItemAction: (formData: FormData) => void
+// }) {
+//   return <form action={updateItemAction}>{/* ... */}</form>
+// }
+
+export default function Searchbar({action}) {
+    // const [state, action] = useActionState(submitSearch, "")
     return (
         <div>
             <form className="flex items-center max-w-lg mx-auto" action={action}>
