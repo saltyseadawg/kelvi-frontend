bec9e3e38d620e125c654f2ea4a5f7cf139a273f
saltyseadawg <sabrinayu7@gmail.com>
2025-11-16 00:20:19 -0500

    - add contact page - add google form for feedback - styling

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,7 @@ export default function About() {
     return (
         <div>
             <Navbar></Navbar>
-            <main className="items-center justify-items-center min-h-screen p-4 pb-20 gap-16 sm:p-20">
+            <main className="items-center justify-items-center min-h-screen p-4 sm:px-38 pb-20 gap-16 ">
                 <h1 className="font-semibold font-serif text-xl text-default-black py-2">About</h1>
                 <p className="py-2">Kelvi (Tamil: கேள்வி) is the passion project of linguist-developers Shankhalika Srikanth and Sabrina Yu. After several years of contributing to community-led language revitalization initiatives together, in 2024 we had the idea of creating a Tamil dictionary specifically geared at helping language learners. Tamil words can get very large, but dictionaries will only carry the “stem” or “root word” form of these words. If you’re not already somewhat familiar with the language, finding the “dictionary-form” of a long Tamil word can feel impossible. Kelvi was designed to 1) provide an easy way for learners to find the “dictionary-form” of any word, and 2) use everyday language to explain what all the complicated-looking suffixes that form the rest of the word mean, so that learners can start to recognize the patterns that go into forming Tamil words themselves. The ultimate goal of Kelvi is to empower learners, and the hope is that teachers and students alike will find it a useful tool.</p>
                 <p className="py-2">Kelvi was made possible through the generous support of the Audrey Wearn Language Prize, the Future Design School, and the contributors listed below. Our sincere thanks as well to everyone who took the time to user test Kelvi while it was in development.</p>
