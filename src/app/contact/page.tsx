323a68f862663855f5e94ccc4c730d2dc6652668
Sorano <sabrinayu7@gmail.com>
2025-11-18 00:37:12 -0500

    Devops/docker (#10)
    * deployment fixes
    
    * add Dockerfile

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,7 +7,7 @@ export default function About(){
                 <div className="items-center justify-items-center min-h-screen p-4 sm:px-38 pb-20 gap-16">
                 <h1 className="font-semibold font-serif text-xl text-default-black py-2">Contact Us</h1>
                 <p className="py-2">We are always looking to improve Kelvi! If you notice any errors or have any suggestions for how Kelvi could be made more useful, we’d love to hear from you.</p>
-                <iframe className="py-4" src="https://docs.google.com/forms/d/e/1FAIpQLSeRux6di2mWrHpxImJmFDrqVcAkMKQ8smFfjBAKRpa40F_4yw/viewform?embedded=true" width="100%" height="1159" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                <iframe className="py-4" src="https://docs.google.com/forms/d/e/1FAIpQLSeRux6di2mWrHpxImJmFDrqVcAkMKQ8smFfjBAKRpa40F_4yw/viewform?embedded=true" width="100%" height="1159">Loading…</iframe>
             </div>
         </div>
     )
