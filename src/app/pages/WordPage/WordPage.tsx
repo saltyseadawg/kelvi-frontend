7afc6e829026a6f8a3af4e14dbed2f445e73f31c
saltyseadawg <sabrinayu7@gmail.com>
2025-12-09 13:31:06 -0500

    group gloss material together

--- src/app/pages/WordPage/WordPage.tsx
+++ src/app/pages/WordPage/WordPage.tsx
@@ -3,37 +3,34 @@
 import styles from './WordPage.module.css'
 
 
-export default function WordPage({data}) {
-    if (data.detail){
-        return (
-          <h2 className="font-sans">Word not found (இ﹏இ`｡)</h2>
-        )
-    } else if (!data.user_input){
+export default function WordPage({ data }) {
+  if (data.detail) {
+    return (
+      <h2 className="font-sans">Word not found (இ﹏இ`｡)</h2>
+    )
+  } else if (!data.user_input) {
 
-    }
-    else {
-        return (
-        <div className={styles.wordpage}>
-            <h1 className="pt-2 text-xl text-default-black font-sans-tamil">{data.user_input}</h1>
-            <h1 className="pb-4 text-xl text-default-black font-sans">{data.romanization}</h1>
-            <hr></hr>
-            <Romanization data={data}/>
-            <hr></hr>
-            <h2 className="indent-2 min-h-0 pt-4 pb-2 text-lg text-default-black font-sans-tamil">
-              {data.root.tamil} ({data.root.romanization})
-            </h2>
-            <ListDefinitions items={data.root_definition}/>
-            <h2 className="indent-2 min-h-0 pt-4 pb-2 text-lg text-default-black font-sans-tamil">
-              {data.suffixal_material.text} ({data.suffixal_material.romanization})
-            </h2>
-            <Gloss data={data}/>
-        </div>
-        )
-    }
+  }
+  else {
+    return (
+      <div className={styles.wordpage}>
+        <h1 className="pt-2 text-xl text-default-black font-sans-tamil">{data.user_input}</h1>
+        <h1 className="pb-4 text-xl text-default-black font-sans">{data.romanization}</h1>
+        <hr></hr>
+        <Romanization data={data} />
+        <hr></hr>
+        <h2 className="indent-2 min-h-0 pt-4 pb-2 text-lg text-default-black font-sans-tamil">
+          {data.root.tamil} ({data.root.romanization})
+        </h2>
+        <ListDefinitions items={data.root_definition} />
+        <Gloss data={data} />
+      </div>
+    )
+  }
 }
 
 function ListDefinitions({ items }) {
-  
+
   return (
     <ol className="list-decimal list-inside space-y-2 text-balance min-h-0 text-base text-default-black font-sans">
       {items.map((entry, index) => (
@@ -47,13 +44,8 @@ function ListDefinitions({ items }) {
   );
 }
 
-function Romanization({ data }){
-  if (data.user_input === data.root.tamil){
-    return (
-      <></>
-    )
-  } else {
-    return(
+function Romanization({ data }) {
+  return (
     <table className="table-auto border-collapse">
       <tbody className="text-lg font-normal">
         <tr className="justify-start font-sans-tamil">
@@ -69,14 +61,25 @@ function Romanization({ data }){
       </tbody>
     </table>
   );
-  }
-  
 }
 
-function Gloss({data}){
-    return(
+function Gloss({ data }) {
+  // nothing to breakdown if user searched up a root form
+  if (data.user_input === data.root.tamil) {
+    return (
+      <></>
+    )
+  } else {
+    return (
+      <div>
+        <h2 className="indent-2 min-h-0 pt-4 pb-2 text-lg text-default-black font-sans-tamil">
+          {data.suffixal_material.text} ({data.suffixal_material.romanization})
+        </h2>
       <pre className="space-y-2 text-balance min-h-0 text-base text-default-black font-sans">
         &emsp;&emsp;{data.suffixal_material.gloss}
       </pre>
+    </div>
+      
     );
   }
+}
