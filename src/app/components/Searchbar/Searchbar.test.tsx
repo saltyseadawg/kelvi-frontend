8047367f517e703836960c637c1021d4e57649d7
saltyseadawg <sabrinayu7@gmail.com>
2026-01-12 20:27:59 -0500

    fix test

--- src/app/components/Searchbar/Searchbar.test.tsx
+++ src/app/components/Searchbar/Searchbar.test.tsx
@@ -1,21 +1,24 @@
 import { describe, expect, test, vi } from 'vitest'
 import { render, screen } from '@testing-library/react'
-import Searchbar from './Searchbar'
-import { submitSearch } from '@/app/actions/actions'
 import { userEvent } from "@testing-library/user-event"
+import Searchbar from './Searchbar'
 
-vi.mock('@/app/actions/actions')
+const pushMock = vi.fn()
 
-describe("submitSearch", () => {
-    test("Normal: Returns the user's input", async () => {
-        render(<Searchbar action={submitSearch}/>)
+vi.mock("next/navigation", () => ({
+  useRouter: () => ({
+    push: pushMock,
+  }),
+  usePathname: () => "/",
+}))
+
+describe("Searchbar", () => {
+    test("Normal: Push user input onto URL", async () => {
+        render(<Searchbar/>)
         expect(screen.getByPlaceholderText("Search a Tamil word")).toBeDefined();
         await userEvent.type(screen.getByLabelText("Searchbar"), "word");
         await userEvent.click(screen.getByRole("button", { name: "Search" }));
-        await vi.waitFor(() =>
-            expect(submitSearch).toHaveBeenCalledOnce()
-        )
-        // not sure why this isn't working
-        // expect(result).toHaveBeenLastCalledWith({ query: "word" }, "", new FormData());
+        expect(pushMock).toHaveBeenCalledTimes(1)
+        expect(pushMock).toHaveBeenCalledWith("/?query=word")
     });
 });
