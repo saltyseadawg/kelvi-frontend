1fb032dc3c2139a26937ebdaee1f30c74c76f8c0
shankhalika-katya <shankha221@gmail.com>
2026-07-14 14:38:48 -0400

    add ACL links

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,9 +10,22 @@ export default function About() {
             <Navbar></Navbar>
             <main className="items-center justify-items-center min-h-screen p-4 sm:px-38 pb-20 gap-16 ">
                 <h1 className="font-semibold font-serif text-xl text-default-black py-2">About</h1>
-                <p className="py-2">Kelvi (Tamil: கேள்வி) is the passion project of linguist-developers Shankhalika Srikanth and Sabrina Yu. After several years of contributing to community-led language revitalization initiatives together, in 2024 we had the idea of creating a Tamil dictionary specifically geared at helping language learners. Tamil words can get very large, but dictionaries will only carry the “stem” or “root word” form of these words. If you’re not already somewhat familiar with the language, finding the “dictionary-form” of a long Tamil word can feel impossible. Kelvi was designed to 1) provide an easy way for learners to find the “dictionary-form” of any word, and 2) use everyday language to explain what all the complicated-looking suffixes that form the rest of the word mean, so that learners can start to recognize the patterns that go into forming Tamil words themselves. The ultimate goal of Kelvi is to empower learners, and the hope is that teachers and students alike will find it a useful tool.</p>
-                <p className="py-2">Kelvi was made possible through the generous support of the Audrey Wearn Language Prize, the Future Design School, and the contributors listed below. Our sincere thanks as well to everyone who took the time to user test Kelvi while it was in development.</p>
-                <p className="py-2">Kelvi is an open-source software, and you can find the source code for the website <a href="https://github.com/saltyseadawg/kelvi-backend" className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600">here</a>.</p>
+                <p className="py-2">Kelvi (Tamil: கேள்வி) is the passion project of linguist-developers Shankhalika Srikanth and Sabrina Yu. 
+                    After several years of contributing to community-led language revitalization initiatives together, 
+                    in 2024 we had the idea of creating a Tamil dictionary specifically geared at helping language learners. 
+                    Tamil words can get very large, but dictionaries will only carry the “stem” or “root word” form of these words. 
+                    If you’re not already somewhat familiar with the language, finding the “dictionary-form” of a long Tamil word can 
+                    feel impossible. Kelvi was designed to 1) provide an easy way for learners to find the “dictionary-form” of any word, 
+                    and 2) use everyday language to explain what all the complicated-looking suffixes that form the rest of the word mean, 
+                    so that learners can start to recognize the patterns that go into forming Tamil words themselves. 
+                    The ultimate goal of Kelvi is to empower learners, and the hope is that teachers and students alike will find it a useful tool.</p>
+                <p className="py-2">Kelvi was made possible through the generous support of the Audrey Wearn Language Prize, 
+                    the Future Design School, and the contributors listed below. 
+                    Our sincere thanks as well to everyone who took the time to user test Kelvi while it was in development.</p>
+                <p className="py-2">Kelvi is an open-source software, and you can find the source code for the website 
+                    <a href="https://github.com/saltyseadawg/kelvi-backend" className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600">here</a>. 
+                    Read more about Kelvi <a href="https://aclanthology.org/2026.bea-1.21/" className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600">here</a> 
+                    or check out our poster for ACL <a href="/Users/shankhalika/kelvi-frontend/src/app/about/BEA_Poster45_Kelvi.pdf" className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600">here</a>!</p>
                 <h1 className="font-semibold font-serif text-xl text-default-black py-4">The Team</h1>
                 <div className="grid sm:grid-cols-2 gap-8 grid-cols-1">
                     {biosJson.map((person) => {
